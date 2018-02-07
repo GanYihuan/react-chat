a151782c62ef0abe8ffbfb447df460fb4bbda5c0
GanYihuan <583520052@qq.com>
2018-02-07 16:55:12 +0800

    redux 4-7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {addGun, removeGun, addGunAsync} from "./index.redux";
 
+@connect(
+    // What attribute do you want state to put into prop?
+    state => ({num: state}),
+    // what function do you want to put into the prop? auto dispatch
+    {addGun, removeGun, addGunAsync}
+)
+
 class App extends Component {
   render() {
     const num = this.props.num;
@@ -16,9 +23,4 @@ class App extends Component {
   }
 }
 
-const mapStatetoProps = (state) => {
-  return {num: state}
-}
-const actionCreators = {addGun, removeGun, addGunAsync};
-App = connect(mapStatetoProps, actionCreators)(App);
 export default App;
