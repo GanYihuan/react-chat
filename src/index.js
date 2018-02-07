46aef1d4e4887edbbc41b3d3bd1dfff4039f12dc
GanYihuan <583520052@qq.com>
2018-02-07 11:19:17 +0800

    format master

--- src/index.js
+++ src/index.js
@@ -4,18 +4,6 @@ import App from './App';
 import {createStore} from 'redux';
 import {counter, addGun, removeGun} from './index.redux';
 
-// reducer
-// Generate new state based on Old State and action
-function counter(state = 0, action) {
-  switch (action.type) {
-    case 'add':
-      return state + 1;
-    case 'decrease':
-      return state - 1;
-    default:
-      return 10;
-  }
-}
 // Data safe
 const store = createStore(counter);
 
@@ -26,11 +14,6 @@ function render() {
   )
 }
 
-store.subscribe(listener);
-// 派发事件，传递action.
-store.dispatch({type: 'add'});
-store.dispatch({type: 'add'});
-store.dispatch({type: 'decrease'});
 render();
 // Subscriptions, data changes will be implemented anew
 store.subscribe(render);
