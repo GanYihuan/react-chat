e7f07a044392fc4cee60fe0ffee03a5aa8ef0364
GanYihuan <583520052@qq.com>
2018-02-07 11:53:33 +0800

    format master

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {counter, addGun, removeGun, addGunAsync} from './index.redux';
 
+
 // Data safe.
 const store = createStore(counter, applyMiddleware(thunk));
 
