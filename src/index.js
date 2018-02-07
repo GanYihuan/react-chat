5931cf195ad38abea6bd5e93f3fbedcb5e35883e
GanYihuan <583520052@qq.com>
2018-02-07 10:37:12 +0800

    redux 4-1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import {createStore} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+// reducer
+// Generate new state based on Old State and action
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'add':
+      return state + 1;
+    case 'decrease':
+      return state - 1;
+    default:
+      return 10;
+  }
+}
+// 新建store
+const store = createStore(counter);
+const init = store.getState();
+console.log(init);
+
+function listener() {
+  const current = store.getState();
+  // Note that the symbol above the TAB key is
+  console.log(`有${current}`);
+}
+
+store.subscribe(listener);
+// 派发事件，传递action
+store.dispatch({type: 'add'});
+store.dispatch({type: 'add'});
+store.dispatch({type: 'decrease'});
