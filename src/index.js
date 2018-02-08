525b8eb7d63e8920f7a3e000ca8a8ac9d603db04
GanYihuan <583520052@qq.com>
2018-02-08 17:16:39 +0800

    format login-page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
-
 import Login from './container/login/login'
 import Register from './container/register/register'
 // checked route
