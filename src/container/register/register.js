37da7e4f760eb93c6eaf9f819d079c83a4a90a21
GanYihuan <583520052@qq.com>
2018-02-08 21:33:41 +0800

    install body-parser

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -32,38 +32,42 @@ class Register extends React.Component {
     const RadioItem = Radio.RadioItem
     return (
         <div>
-          {this.props.redirectTo ? <Redirect to={this.props.redirectTo}/> : null}
+          {
+            this.props.redirectTo
+                ?
+                <Redirect to={this.props.redirectTo}/>
+                :
+                null
+          }
           <Logo/>
           <List>
-            {this.props.msg ? <p className='error-msg'>{this.props.msg}</p> : null}
+            {
+              this.props.msg
+                  ?
+                  <p className='error-msg'>{this.props.msg}</p>
+                  :
+                  null
+            }
             <InputItem onChange={v => this.handleChange('user', v)}>用户名</InputItem>
             <WhiteSpace/>
             <InputItem
                 type='password'
                 onChange={v => this.handleChange('pwd', v)}
-            >
-              密码
-            </InputItem>
+            >密码</InputItem>
             <WhiteSpace/>
             <InputItem
                 type='password'
                 onChange={v => this.handleChange('repeatpwd', v)}
-            >
-              确认密码
-            </InputItem>
+            >确认密码</InputItem>
             <WhiteSpace/>
             <RadioItem
                 checked={this.state.type == 'genius'}
                 onChange={() => this.handleChange('type', 'genius')}
-            >
-              牛人
-            </RadioItem>
+            >牛人</RadioItem>
             <RadioItem
                 checked={this.state.type == 'boss'}
                 onChange={() => this.handleChange('type', 'boss')}
-            >
-              BOSS
-            </RadioItem>
+            >BOSS</RadioItem>
             <WhiteSpace/>
             <Button type='primary' onClick={this.handleRegister}>注册</Button>
           </List>
