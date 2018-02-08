156c9946c5298722a4497496f4828856b6f85294
GanYihuan <583520052@qq.com>
2018-02-08 17:00:19 +0800

    login-page 6-2

--- src/index.js
+++ src/index.js
@@ -1,53 +1,37 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import { 
-	BrowserRouter, 
-	Route, 
-	Redirect,
-	Switch
-} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
 
+import Login from './container/login/login'
+import Register from './container/register/register'
+// checked route
+import AuthRoute from './component/authroute/authroute'
 import reducers from './reducer'
-import Auth from './Auth.js'
-import Dashboard from './Dashboard'
 import './config'
-import 'antd-mobile/dist/antd-mobile.css'
+import './index.css'
 
 const store = createStore(reducers, compose(
-	applyMiddleware(thunk),
-	window.devToolsExtension?window.devToolsExtension():f=>f
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
 ))
 
-// class Test extends React.Component{
-// 	constructor(props) {
-// 		super(props)
-		
-// 	}
-// 	render(){
-// 		console.log(this.props)
-// 		return <h2>测试组件 {this.props.match.params.location}</h2>
-// 	}
-// }
+function Boss() {
+  return <h2>BOSS页面</h2>
+}
 
-// 登录
-// 	没有登录信息 统一跳转login.
-// 页面  导航+显示+注销
-// 	一营
-// 	二营
-// 	骑兵连
-// router+redux
 ReactDom.render(
-	(<Provider store={store}>
-		<BrowserRouter>
-			<Switch>
-					<Route path='/login' component={Auth}></Route>
-				  <Route path='/dashboard' component={Dashboard}></Route>
-				  <Redirect to='/dashboard'></Redirect>
-			</Switch>
-		</BrowserRouter>
-	</Provider>),
-	document.getElementById('root')
+    (<Provider store={store}>
+      <BrowserRouter>
+        <div>
+          <AuthRoute/>
+          <Route path='/boss' component={Boss}></Route>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/register' component={Register}></Route>
+        </div>
+      </BrowserRouter>
+    </Provider>),
+    document.getElementById('root')
 )
