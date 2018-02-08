f6978bd204d9ef7f4f37b15b25cc90e46205776c
GanYihuan <583520052@qq.com>
2018-02-08 17:44:49 +0800

    login-page 6-4

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {Provider} from 'react-redux'
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
 import Login from './container/login/login'
 import Register from './container/register/register'
-// checked route
+// checked route.
 import AuthRoute from './component/authroute/authroute'
 import reducers from './reducer'
 import './config'
