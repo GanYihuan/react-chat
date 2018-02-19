d915489500e8353d145707001f3c5141fd6cb291
GanYihuan <583520052@qq.com>
2018-02-20 01:04:09 +0800

    3-1 basic

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
 import React from 'react'
-import ReactDom from 'react-dom'
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
-import {Provider} from 'react-redux'
-import {BrowserRouter, Route} from 'react-router-dom'
-import Login from './container/login/login'
-import Register from './container/register/register'
-// checked route
-import AuthRoute from './component/authroute/authroute'
-import reducers from './reducer'
-import './config'
+import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css'
 
-const store = createStore(reducers, compose(
-    applyMiddleware(thunk),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-))
-
-function Boss() {
-  return <h2>BOSS页面</h2>
-}
-
-ReactDom.render(
-    (<Provider store={store}>
-      <BrowserRouter>
-        <div>
-          <AuthRoute/>
-          <Route path='/boss' component={Boss}></Route>
-          <Route path='/login' component={Login}></Route>
-          <Route path='/register' component={Register}></Route>
-        </div>
-      </BrowserRouter>
-    </Provider>),
+ReactDOM.render(
+    <App/>,
     document.getElementById('root')
-)
+);
