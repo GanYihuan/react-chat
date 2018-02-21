23ea756abe308cacc6a1a0446dbcfdb1c7bedfd0
GanYihuan <583520052@qq.com>
2018-02-21 21:45:08 +0800

    4-12 redux-router4

--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,5 @@
 import {combineReducers} from 'redux'
 import {counter} from './redux-2'
 import {auth} from './Auth.redux' // 4-11
-// import {user} from './redux/user.redux'
 
 export default combineReducers({counter, auth})
-// export default combineReducers({user})
