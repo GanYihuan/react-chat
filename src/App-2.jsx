3c2b8436963f69683e453a5f104929a8e4b26b76
GanYihuan <583520052@qq.com>
2018-08-19 09:36:37 +0800

    4-7 Redux 状态管理 7-使用 React-redux（Connect 可以用装饰器的方法来书写）

--- src/App-2.jsx
+++ src/App-2.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 /* 从外部组件获取参数 */
 import { connect } from 'react-redux'
 import { Button } from 'antd-mobile'
 import { addGun, removeGun, addGunAsync } from './redux'
 
-// /* adorn mode */
-// @connect(
-// 	/*
-//   matStatetoProps
-//   state put into props
-//   */
-// 	state => ({ num: state.counter }),
-// 	/*
-//   actionCreators
-//   func into props
-//   */
-// 	{ addGun, removeGun, addGunAsync }
-// )
-class App extends React.Component {
+/* adorn mode */
+/*
+@connect replace it
+state put into props
+*/
+// const mapStatetoProps = state => {
+// 	return { num: state }
+// }
+/*
+addGun, removeGun, addGunAsync 
+func put into props
+*/
+// const actionCreators = { addGun, removeGun, addGunAsync }
+@connect(
+	state => ({ num: state }),
+	{ addGun, removeGun, addGunAsync }
+)
+class App extends Component {
 	render() {
 		const { num, addGun, removeGun, addGunAsync } = this.props
 		return (
@@ -31,21 +35,8 @@ class App extends React.Component {
 	}
 }
 
-/*
-@connect replace it
-state put into props
-*/
-const mapStatetoProps = state => {
-	return { num: state }
-}
-
-/*
-addGun, removeGun, addGunAsync 
-func put into props
-*/
-const actionCreators = { addGun, removeGun, addGunAsync }
-
-export default connect(
-	mapStatetoProps,
-	actionCreators
-)(App)
+// export default connect(
+// 	mapStatetoProps,
+// 	actionCreators
+// )(App)
+export default App
